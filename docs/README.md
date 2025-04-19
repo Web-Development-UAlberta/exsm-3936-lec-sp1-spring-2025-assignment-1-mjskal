2911a18d3b317a45df602dbfc2a5c2f060326192
mjskal <matthew.james.skal@gmail.com>
2025-04-19 12:29:30 -0700

    Created car/engine classes, instantiated new car, tested properties;

--- docs/README.md
+++ docs/README.md
@@ -1,10 +1,30 @@
-# University of Alberta Browser Console Template Repository
-## Full Stack Web Application Development Program
-### This repository is intended for the development of console JavaScript applications.
+# ASSIGNMENT 1 - JAVASCRIPT II - OBJECT-ORIENTED PROGRAMMING
 
-Run `npm run lint` to attempt to detect errors in your repository.
+- [PROJECT DESCRIPTION](#project-description)
+- [TECHNOLOGY STACK](#technology-stack)
+- [FUTURE IMPLEMENTATIONS AND IMPROVEMENTS](#future-implementations-and-improvements)
+- [KNOWN BUGS](#known-bugs)
+- [INSTALLATION AND SETUP](#installation-and-setup)
+- [DETAILED USER MANUAL](#detailed-user-manual)
+- [CREDITS AND ACKNOWLEDGMENTS](#credits-and-acknowledgments)
 
-Use `output()` and `await input()` from `./src/js/scripts.js` to allow the user to interact with the console.
-To run the console, launch `./src/index.html`.
+# PROJECT DESCRIPTION
 
-Add your README content here.
+
+# TECHNOLOGY STACK
+_Apps and Versions are listed as follows: GIT HUB Version 3.14, VISUAL STUDIO CODE Version: 1.98.2, OS Mac Sonoma 14.7.4, and GIT Version 2.39.5 (Apple Git-154)._
+
+# FUTURE IMPLEMENTATIONS AND IMPROVEMENTS
+_There are no further implementations planned._
+
+# KNOWN BUGS
+_There are no known bugs in this assignment._
+
+# INSTALLATION AND SETUP
+_LAUNCH index.html from src folder._ 
+
+# DETAILED USER MANUAL
+_Non-applicable._
+
+# CREDITS AND ACKNOWLEDGMENTS
+_The University of Alberta is credited with the creation of the original online repository._
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,14 +1,44 @@
-// eslint-disable-next-line no-unused-vars
-/* global output, input */
-// eslint-disable-next-line no-unused-vars
 async function main() {
-  // This is where the code you're actually experimenting with goes.
 
-  const prompt = "Please enter your name, or 'Exit' to quit: ";
-  let name = await input(prompt);
+//Two classes for car and engine with their own constructors 
 
-  while (name !== "Exit") {
-    output("Hello, " + name + "!");
-    name = await input(prompt);
-  }
+    //Blueprint for a car
+    class Car {
+        constructor(make, model, year){
+            this.make = make; 
+            this.model = model; 
+            this.year = year; 
+            this.odometer = 0;
+            this.engine = new Engine(6, false);  
+        }
+    }
+    
+    //Blueprint for engine
+    class Engine {
+        constructor(cylinderCount, isRunning){
+            this.cylinderCount = cylinderCount;
+            this.isRunning = isRunning; 
+        }
+    }
+
+//1. Instantiate a car
+//Value for odometer property is always 0, value for engine property is a new nested object of engine class with 6 cylinders and engine false
+
+let myCar = new Car("Honda", "CRV", 2025); 
+
+//Test outputs 
+
+output(`Car make: ${myCar.make}`); 
+output(`Car model: ${myCar.model}`); 
+output(`Car year: ${myCar.year}`); 
+output(`Car odometer: ${myCar.odometer}`); 
+output(`Engine cylinders: ${myCar.engine.cylinderCount}`); 
+output(`Is the car running: ${myCar.engine.isRunning}`); 
+
 }
+
+// 2. Turn its engine on
+// 3. Drive for 100 KM. 
+// 4. Turn engine off.  5. Turn engine on.  6. Drive for 50KM. 7. Turn engine off. 
+// 8. Output the odometer.   
+// 9. Output JSON string representing car object to console.
